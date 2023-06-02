bf3ee2f6fc51ff057bfee9c98041dca2c0a2beb3
Ben Stone <bw.stone9@gmail.com>
2023-06-02 12:38:38 -0400

    fix absent bcftools consensus "-n"

--- 3_variant_calling_and_outfile_generation/VCFcall_4a.ARRAY_generate_consensus_fullgenome.sh
+++ 3_variant_calling_and_outfile_generation/VCFcall_4a.ARRAY_generate_consensus_fullgenome.sh
@@ -74,7 +74,7 @@ scaffoldnames=$(awk '{print $1}' $faidxfile)
 
 samtools faidx $refgenome ${scaffoldnames[@]} | 
  bcftools consensus --haplotype I \
- --absent "-" \
+ --absent "N" \
  --missing "N" \
  --mark-del "-" \
  --sample $infile \
